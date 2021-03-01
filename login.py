35c8bef02d73f986d9ceb9f1d8bf056c1c9ea235
27조 <79817453+unkwn22@users.noreply.github.com>
2021-03-01 22:59:09 +0900

    second commit

--- login.py
+++ login.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, jsonify, request, session
 
 app = Flask(__name__)
 
 from pymongo import MongoClient
+
+import bcrypt
 
 client = MongoClient('localhost', 27017)
 db = client.accountdata
@@ -13,25 +15,26 @@ db = client.accountdata
 
 ## HTML 화면 보여주기
 @app.route('/')
-def login_page():
+def main_page():
     return render_template('login.html')
 
-
-@app.route('/login', methods=['CHECK'])
-def check_id():
+#함수
+@app.route('/login', methods=['POST'])
+def login():
     id_receive = request.form['id_give']
     pass_receive = request.form['pass_give']
+    existing_user = user = db.users.find_one({'id': id_receive})
 
-    user = db.users.find_one({'id': id_receive})
-
-
-    if user is not None:
-        # if pass_receive is not
+    if existing_user is not None:
+        password = user['pass']
+        if pass_receive == password:
+            return jsonify({'check': 1})
+        else:
+            print("Invalid Password")
+            return jsonify({'check': 3})
     else:
-        print("없는 아이디입니다")
-
-    return jsonify({'msg': '하이!'})
-
+        print("Invalid Username")
+        return jsonify({'check': 2})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
